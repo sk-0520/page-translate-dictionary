43daa2c287d2ed69cf69de34584a500eb8899a69
sk <content.type.text.net@gmail.com>
2022-10-21 00:03:26 +0900

    dom 試験開始

--- source/test/dom.test.ts
+++ source/test/dom.test.ts
@@ -1,15 +1,20 @@
+/**
+ * @jest-environment jsdom
+ */
 import * as dom from '../scripts/dom';
 
 describe('dom', () => {
-	// test('requireElementById', () => {
-	// 	document.body.innerHTML = `
-	// 		<span id="id1">id1:1</span>
-	// 		<span id="id2">id2:1</span>
-	// 	`;
+	test('requireElementById', () => {
+		document.body.innerHTML = `
+			<span id="id1">id1:1</span>
+			<span id="id2">id2:1</span>
+		`;
 
-	// 	expect(dom.requireElementById('id1').textContent!).toBe('id1:1');
-	// 	expect(dom.requireElementById('id2').textContent!).toBe('id2:1');
-	// });
+		expect(dom.requireElementById('id1').textContent!).toBe('id1:1');
+		expect(dom.requireElementById('id2').textContent!).toBe('id2:1');
+
+		expect(() => dom.requireElementById('id3')).toThrowError(Error);
+	});
 
 	test('toCustomKey', () => {
 		expect(dom.toCustomKey('')).toBe('');
