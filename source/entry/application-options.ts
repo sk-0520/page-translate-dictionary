13d8c68f7a3943206a2c583e8302b7176bd25729
sk <content.type.text.net@gmail.com>
2022-10-26 20:05:41 +0900

    app -> extension

--- source/entry/application-options.ts
+++ source/entry/application-options.ts
@@ -1,3 +1,3 @@
-import * as options from '../scripts/app/options/options';
+import * as options from '../scripts/extension/options/options';
 
 options.boot();
--- source/entry/page-content.ts
+++ source/entry/page-content.ts
@@ -1,3 +1,3 @@
-import * as page from '../scripts/app/page/page';
+import * as page from '../scripts/extension/page/page';
 
 page.boot();
