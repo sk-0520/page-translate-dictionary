930d4c98dc47c0049519739430340d0e2a4be74b
sk <content.type.text.net@gmail.com>
2022-11-07 04:17:18 +0900

    不要ファイル破棄

--- chrome/windows.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set CHROME=C:\Program Files\Google\Chrome\Application\chrome.exe
-
-set CUSTOM_BATCH_FILE=@windows.bat
-if exist "%CUSTOM_BATCH_FILE%" call "%CUSTOM_BATCH_FILE%"
-
-"%CHROME%" %*
-
