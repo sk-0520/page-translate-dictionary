c5c9443be346ca2f455d01ed5bf129b19cafe40a
sk <content.type.text.net@gmail.com>
2022-11-06 19:33:45 +0900

    色々おかしいけど chrome でも一応動いたわ

---
+++ source/entry/application-options@chrome.ts
@@ -0,0 +1,4 @@
+import * as options from '../scripts/extension/options/options';
+import * as extensions from '../scripts/extension/extensions';
+
+options.boot(new extensions.Extension(extensions.BrowserKind.Chrome));
---
+++ source/entry/popup-action@chrome.ts
@@ -0,0 +1,4 @@
+import * as action from '../scripts/extension/action/action';
+import * as extensions from '../scripts/extension/extensions';
+
+action.boot(new extensions.Extension(extensions.BrowserKind.Chrome));
