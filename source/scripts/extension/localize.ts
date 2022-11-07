c3ab48858b98b2cafe39610b75806116ee2294eb
sk <content.type.text.net@gmail.com>
2022-11-07 23:03:15 +0900

    とりあえずの記号定数化

--- source/scripts/extension/localize.ts
+++ source/scripts/extension/localize.ts
@@ -1,6 +1,12 @@
 import webextension from "webextension-polyfill";
 import * as string from "../core/string";
 
+const base = 'data-ext';
+const LocalizeAttribute = {
+	start: base + '-',
+	text: base + '-text',
+} as const;
+
 /**
  * 拡張機能の要素に対してローカライズを実施。
  *
@@ -11,31 +17,30 @@ import * as string from "../core/string";
  */
 export function applyElement(element: Element): void {
 	for (const attribute of element.attributes) {
-		if (!attribute.name.startsWith('data-ext-')) {
+		if (!attribute.name.startsWith(LocalizeAttribute.start)) {
 			continue;
 		}
 
-		if (attribute.name === 'data-ext-text') {
+		if (attribute.name === LocalizeAttribute.text) {
 			const message = webextension.i18n.getMessage(attribute.value);
 			if (!string.isNullOrWhiteSpace(message)) {
 				element.textContent = message;
 			}
 		} else {
-			const attrName = attribute.name.substring('data-ext-'.length);
+			const attrName = attribute.name.substring(LocalizeAttribute.start.length);
 			const message = webextension.i18n.getMessage(attribute.value);
 			if (!string.isNullOrWhiteSpace(message)) {
 				element.setAttribute(attrName, message);
 			}
 		}
 	}
-
 }
 
 /**
  * 拡張機能のビューに対してローカライズを実施。
  */
 export function applyView(): void {
-	const elementList = document.querySelectorAll<HTMLElement>('*');
+	const elementList = document.querySelectorAll('*');
 	for (const element of elementList) {
 		applyElement(element);
 	}
