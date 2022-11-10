33d8bde8633e674be6246f424b640f7f7ffc1de6
sk <content.type.text.net@gmail.com>
2022-11-10 23:12:46 +0900

    as const

--- source/scripts/extension/names.ts
+++ source/scripts/extension/names.ts
@@ -4,11 +4,11 @@ const baseAttr = 'data-' + baseName + '-';
 export const ClassNames = {
 	progress: baseName + '-progress',
 	mark: baseName + '-marked',
-}
+} as const;
 
 export const Attributes = {
 	translated: baseAttr + 'translated',
 	translateSettings: baseAttr + 'setting',
 	textHead: baseAttr + 'translated-text-',
 	attributeHead: baseAttr + 'translated-attr-',
-}
+} as const;
