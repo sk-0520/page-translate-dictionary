44a698cd3c23b0bd4523e15de37ce30b47e0c7a7
sk <content.type.text.net@gmail.com>
2022-12-05 20:10:07 +0900

    なんかした

--- source/scripts/core/logging.ts
+++ source/scripts/core/logging.ts
@@ -1,5 +1,11 @@
-type LoggingMethod = (message?: any, ...optionalParams: any[]) => void;
+/**
+ * 通常ログメソッド。
+ */
+export type LogMethod = (message?: any, ...optionalParams: any[]) => void;
 
+/**
+ * ログレベル。
+ */
 export enum LogLevel {
 	Trace,
 	Debug,
@@ -9,68 +15,82 @@ export enum LogLevel {
 	Error,
 }
 
+export interface LogOptions {
+	//#region property
+
+	level: LogLevel;
+
+	//#endregion
+
+	//#region function
+
+
+	//#endregion
+}
+
 export interface Logger {
-	trace: LoggingMethod;
-	debug: LoggingMethod;
-	log: LoggingMethod;
-	info: LoggingMethod;
-	warn: LoggingMethod;
-	error: LoggingMethod;
+	readonly level: LogLevel;
+	readonly header: string;
+
+	/** `LogLevel.Trace` 以上のログ出力処理 */
+	trace: LogMethod;
+	/** `LogLevel.Debug` 以上のログ出力処理 */
+	debug: LogMethod;
+	/** `LogLevel.Log` 以上のログ出力処理 */
+	log: LogMethod;
+	/** `LogLevel.Information` 以上のログ出力処理 */
+	info: LogMethod;
+	/** `LogLevel.Warning` 以上のログ出力処理 */
+	warn: LogMethod;
+	/** `LogLevel.Error` 以上のログ出力処理 */
+	error: LogMethod;
 }
 
-export function create(header: string, level: LogLevel): Logger {
-	return new LoggerImpl(header, level);
+export function nop(message?: any, ...optionalParams: any[]): void {
 }
 
-class LoggerImpl implements Logger {
-	public constructor(private readonly header: string, private readonly level: LogLevel) {
-		const logHeader = '[' + this.header + ']';
+export function toMethod(currentLevel: LogLevel, targetLevel: LogLevel, method: LogMethod): LogMethod {
+	return currentLevel <= targetLevel
+		? method
+		: nop
+		;
+}
 
-		this.trace = this.level <= LogLevel.Trace
-			? console.trace.bind(console, logHeader)
-			: LoggerImpl.nop
-			;
+export function create(header: string, options: LogOptions): Logger {
+	return new ConsoleLogger(header, options);
+}
 
-		this.debug = this.level <= LogLevel.Debug
-			? console.debug.bind(console, logHeader)
-			: LoggerImpl.nop
-			;
+class ConsoleLogger implements Logger {
+	public constructor(public readonly header: string, private readonly options: LogOptions) {
+		const logHeader = '[' + this.header + '] ';
 
-		this.log = this.level <= LogLevel.Log
-			? console.log.bind(console, logHeader)
-			: LoggerImpl.nop
-			;
-
-		this.info = this.level <= LogLevel.Information
-			? console.info.bind(console, logHeader)
-			: LoggerImpl.nop
-			;
-
-		this.warn = this.level <= LogLevel.Warning
-			? console.warn.bind(console, logHeader)
-			: LoggerImpl.nop
-			;
-
-		this.error = this.level <= LogLevel.Error
-			? console.error.bind(console, logHeader)
-			: LoggerImpl.nop
-			;
+		this.trace = toMethod(options.level, LogLevel.Trace, console.trace.bind(console, logHeader));
+		this.debug = toMethod(options.level, LogLevel.Debug, console.debug.bind(console, logHeader));
+		this.log = toMethod(options.level, LogLevel.Log, console.log.bind(console, logHeader));
+		this.info = toMethod(options.level, LogLevel.Information, console.info.bind(console, logHeader));
+		this.warn = toMethod(options.level, LogLevel.Warning, console.warn.bind(console, logHeader));
+		this.error = toMethod(options.level, LogLevel.Error, console.error.bind(console, logHeader));
 	}
 
-	//#region Logger
+	//#region property
 
-	private static nop(message?: any, ...optionalParams: any[]): void { }
+	//#endregion
 
+	//#region function
 	//#endregion
 
 	//#region Logger
 
-	trace: LoggingMethod;
-	debug: LoggingMethod;
-	log: LoggingMethod;
-	info: LoggingMethod;
-	warn: LoggingMethod;
-	error: LoggingMethod;
+	public get level(): LogLevel {
+		return this.options.level;
+	}
+
+	trace: LogMethod;
+	debug: LogMethod;
+	log: LogMethod;
+	info: LogMethod;
+	warn: LogMethod;
+	error: LogMethod;
 
 	//#endregion
 }
--- source/scripts/extension/log.ts
+++ source/scripts/extension/log.ts
@@ -6,10 +6,15 @@ import * as logging from '../core/logging';
 export function create(header: string): logging.Logger {
 	const isProduction = true;
 
-	return logging.create(
-		header,
-		isProduction
+	const options: logging.LogOptions = {
+		level: isProduction
 			? logging.LogLevel.Information
 			: logging.LogLevel.Trace
+		,
+	};
+
+	return logging.create(
+		header,
+		options
 	)
 }
